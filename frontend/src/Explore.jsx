66f235daa670fe241e1ef57a9b406e182c3a7ffd
ICP.Ninja <noreply@icp.ninja>
2025-03-24 11:34:20 +0700

    ICP.Ninja export

---
+++ frontend/src/Explore.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from './Link'
+
+const Explore = () => {
+  return (
+    <div>
+      <h1>Explore Page</h1>
+      <Link href="/">Back to home page</Link>
+    </div>
+  );
+};
+
+export default Explore;
