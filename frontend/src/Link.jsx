d4c26702fb7b905454917f7bdcba72e4e8311bee
ICP.Ninja <noreply@icp.ninja>
2025-03-24 11:34:22 +0700

    ICP.Ninja export

---
+++ frontend/src/Link.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useRouter } from "./Router";
+
+const Link = ({ href, children, ...props }) => {
+  const { navigate } = useRouter();
+
+  return (
+    <a
+      href={href}
+      onClick={(e) => {
+        e.preventDefault();
+        navigate(href);
+      }}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
